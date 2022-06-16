ca16b4d62b47a94868bd5c843f46dec5adaedc79
bataltsevV <mr.podstrelennyy@mail.ru>
2022-06-16 18:17:33 +0300

    changed method for test

--- src/test/java/ru/netology/sqr/TestsForBordersCalculator.java
+++ src/test/java/ru/netology/sqr/TestsForBordersCalculator.java
@@ -2,12 +2,11 @@ package ru.netology.sqr;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
 public class TestsForBordersCalculator {
 
     @ParameterizedTest
-    @CsvFileSource(files="src/test/resources/TestBelongToInterval.csv")
+    @CsvFileSource(resources = "/data.csv")
     public void testBelongToInterval(int expected, int firstNumber, int secondNumber){
             SQRService service = new SQRService();
             int actual = service.calculate(firstNumber, secondNumber);
@@ -16,7 +15,7 @@ public class TestsForBordersCalculator {
 
     }
     @ParameterizedTest
-    @CsvFileSource(files="src/test/resources/TestBelongToInvertedInterval.csv")
+    @CsvFileSource(files = "src/test/resources/TestBelongToInvertedInterval.csv")
     public void testBelongToInvertedInterval(int expected, int firstNumber, int secondNumber){
         SQRService service = new SQRService();
         int actual = service.calculate(firstNumber, secondNumber);
